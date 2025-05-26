c7f94b2ba42ffc4e6122534310850f724d6e0c61
Otabekcdr <boltaboyevotabek207@gmail.com>
2025-05-26 20:15:25 +0500

    start

---
+++ js/main.js
@@ -0,0 +1,4 @@
+        document.querySelector('.menu-button').addEventListener('click', function () {
+            document.querySelector('.nav-mobile').classList.toggle('active');
+            this.classList.toggle('active');
+        });
